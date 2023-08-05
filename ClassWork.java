1f22e23f0691910c2a85786b8a3775e63c4e6a8f
Uexcel2022 <121309125+Uexcel2022@users.noreply.github.com>
2023-08-05 23:00:52 +0100

    first commit

---
+++ ClassWork.java
@@ -0,0 +1,82 @@
+package RegularExpression;
+
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ClassWork {
+
+	public ClassWork() {
+		String word = "abbbbcccdddeeeefbddccrreee";
+		char[] toCharArray = word.toCharArray();
+		String string = "";
+		String add = "";
+		int i = 1;
+		int lenght = toCharArray.length;
+
+		for (; i < lenght; i++) {
+
+			if (toCharArray[i] == toCharArray[i - 1]) {
+				add = String.valueOf(toCharArray[i]);
+				string += toCharArray[i - 1];
+			} else {
+				if (string.contains(add)) {
+					string += add;
+					add = " ";
+					string += " ";
+				}
+			}
+
+		}
+		if ( String.valueOf(word.charAt(word.length() - 1)).equals(String.valueOf(string.charAt(string.length() - 1))) ) {
+			string += String.valueOf(word.charAt(word.length() - 1));
+		}
+
+		String[] wordArray = string.split(" ");
+		String filterWordArray = "";
+		for (int J = 0; J < wordArray.length; J++) {
+			if (!wordArray[J].isEmpty()) {
+				filterWordArray += wordArray[J];
+				filterWordArray += " ";
+			}
+
+		}
+
+		System.out.println("String: " + word);
+		System.out.println("\nBuilded string: " + filterWordArray + "\n");
+
+		String[] filteredWordArray = filterWordArray.split(" ");
+//		String[] filteredWordArray = {"bbbb*", "cc*", "ddd*", "eee*", "r*"};
+		String controlString = "";
+
+		String regex = "";
+		for (String n : filteredWordArray) {
+
+			if (!controlString.contains(n)) {
+				controlString += n;
+				if (n.length() >= 3) {
+					regex = String.format("%s*", String.valueOf(n.charAt(0)) + String.valueOf(n.subSequence(1, 3)));
+				} else {
+					regex = String.format("%s*", n);
+				}
+				Pattern pattern = Pattern.compile(regex);
+				Matcher matcher = pattern.matcher(word);
+				while (matcher.find()) {
+					if (!matcher.group().isEmpty()) {
+						System.out.println(matcher.group() + "\nStarts from index :" + matcher.start()
+								+ "\nend at index: " + matcher.end());
+					}
+				}
+			}
+		}
+
+	}
+
+	public static void main(String[] args) {
+		new ClassWork();
+	}
+
+}
+
+//			
+//			
